d91099bf1b19344d80b61660cb1a50cdc99edf6c
Ohad Malichi <ohad.malichi@gmail.com>
2021-04-11 04:28:26 -0700

    1st version for debian and ubuntu

---
+++ ubuntu-get-k8s-tools.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+cd /tmp
+
+# general
+
+sudo apt update
+sudo apt install docker.io curl -y
+sudo systemctl enable docker.service
+
+# minikube
+
+ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+ sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# kubectl
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo install kubectl /usr/local/bin/kubectl
+
+
+# helm
+
+curl -LO https://get.helm.sh/helm-v3.5.3-linux-amd64.tar.gz
+
+tar -xzf helm-v3.5.3-linux-amd64.tar.gz
+
+sudo install linux-amd64/helm /usr/local/bin/helm
+
+
+echo
+echo kubectl version:
+echo -------------------
+
+kubectl version
+
+echo
+echo minikube version:
+echo -------------------
+
+minikube version
+
+echo
+echo helm version:
+echo -------------------
+
+helm version
+
+echo
+
