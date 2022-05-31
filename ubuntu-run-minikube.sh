e420c017f37777122d9e259c3fd2281313a18fa0
Ohad Malichi <ohad.malichi@gmail.com>
2022-05-31 11:43:19 +0300

    Update ubuntu-run-minikube.sh

--- ubuntu-run-minikube.sh
+++ ubuntu-run-minikube.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo apt update
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+sudo apt -y update
+
 sudo apt install docker.io -y
+sudo apt install -y kubectl
 
 cat /etc/group | grep docker | grep $USERNAME &>/dev/null
 
