cff2da0351a28317b787e98656f58b0accd32a96
Ohad Malichi <ohad.malichi@gmail.com>
2022-07-07 12:43:56 +0300

    Update ubuntu-run-minikube.sh

--- ubuntu-run-minikube.sh
+++ ubuntu-run-minikube.sh
@@ -14,40 +14,56 @@ sudo install linux-amd64/helm /usr/local/bin/helm
 
 cat /etc/group | grep docker | grep $USERNAME &>/dev/null
 
-if ! [ "$?" -eq 0 ]; then
-	# call yourself back after adding the user to the docker group
-	sudo adduser $USERNAME docker
-	$0
-	# exit current run
-	exit
-fi
-
 sudo service docker start
 
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo service docker status --no-pager
 
-minikube start --driver=docker
+if [ "$?" -eq 0 ]; then
+    cat /etc/group | grep docker | grep $USERNAME &>/dev/null
 
-if ! [ -s "/etc/bash_completion.d/kubectl" ]; then
-	git clone https://github.com/ohadm2/k8s-tools-installer.git
-	
-	cd k8s-tools-installer
+    if ! [ "$?" -eq 0 ]; then
+      # call yourself back after adding the user to the docker group
+      sudo usermod -a -G docker $USERNAME
+      sudo $0
+      # exit current run
+      exit
+    fi
 
-	kubectl completion bash > /tmp/kubectl_completion
-	sudo install /tmp/kubectl_completion /etc/bash_completion.d/kubectl
-	sudo install complete_alias.sh /etc/bash_completion.d/
+    if ! [ -s "/usr/local/bin/minikube" ]; then
+      curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+      sudo install minikube-linux-amd64 /usr/local/bin/minikube
+    fi
+    
+    if ! [ -x "/usr/local/bin/minikube" ]; then
+        minikube start --driver=docker
+    fi
+    
+    if ! [ -s "/etc/bash_completion.d/kubectl" ]; then
+      git clone https://github.com/ohadm2/k8s-tools-installer.git
+      
+      cd k8s-tools-installer
 
-	cp ~/.bashrc ~/bashrc-backup-$(date +%s)
-	cp bashrc-for-k8s ~/.bashrc
-	
-	/etc/bash_completion.d/kubectl
-	/etc/bash_completion.d/complete_alias.sh
-	
-	. ~/.bashrc
+      kubectl completion bash > /tmp/kubectl_completion
+      sudo install /tmp/kubectl_completion /etc/bash_completion.d/kubectl
+      sudo install complete_alias.sh /etc/bash_completion.d/
+
+      cp ~/.bashrc ~/bashrc-backup-$(date +%s)
+      cp bashrc-for-k8s ~/.bashrc
+      
+      /etc/bash_completion.d/kubectl
+      /etc/bash_completion.d/complete_alias.sh
+      
+      . ~/.bashrc
+    fi
+    
+    echo "tests:"
+    
+    kubectl get pods
+    helm
+
+    echo "Run 'source ~/.bashrc' and then 'h' for help. Also run 'alias' to view the new aliases."
+else
+    echo "Error Occurred while trying to run the docker daemon! Aborting ..."
 fi
-
-echo "Run 'source ~/.bashrc' and then 'h' for help. Also run 'alias' to view the new aliases."
-
 
 
