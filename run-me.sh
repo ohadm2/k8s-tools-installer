c2777313889a5d0c7e818e908f77ad35512ffaac
Ohad Malichi <ohad.malichi@gmail.com>
2022-08-25 10:31:17 +0300

    Update run-me.sh

--- run-me.sh
+++ run-me.sh
@@ -1,1 +1,1 @@
-sudo curl -k https://raw.githubusercontent.com/ohadm2/k8s-tools-installer/master/ubuntu-get-k8s-tools.sh | bash
+sudo -E curl -k https://raw.githubusercontent.com/ohadm2/k8s-tools-installer/master/ubuntu-get-k8s-tools.sh | bash
