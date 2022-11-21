93b83db563d35e97e3ee4112ed3f8952a4b8ee74
Ohad Malichi <ohad.malichi@gmail.com>
2022-11-21 10:40:27 -0500

    Update runme-minikube-ubuntu.sh

--- runme-minikube-ubuntu.sh
+++ runme-minikube-ubuntu.sh
@@ -1,1 +1,2 @@
-https://raw.githubusercontent.com/ohadm2/k8s-tools-installer/master/minikube-in-ubuntu.sh
+sudo apt update && apt install curl -y
+sudo -E curl -k https://raw.githubusercontent.com/ohadm2/k8s-tools-installer/master/minikube-in-ubuntu.sh | bash
